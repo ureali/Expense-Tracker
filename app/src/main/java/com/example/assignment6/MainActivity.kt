bc418c65673b3a46eff1c1393a5329d6f9ff2438
“Stanislav <w0859305@myscc.ca>
2025-02-25 15:24:53 -0500

    Finish Assignment 6

--- app/src/main/java/com/example/assignment6/MainActivity.kt
+++ app/src/main/java/com/example/assignment6/MainActivity.kt
@@ -1,20 +1,38 @@
 package com.example.assignment6
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var nameEditText: EditText
+    private lateinit var nameOutputTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        nameEditText = findViewById(R.id.editTextText)
+        nameOutputTextView = findViewById(R.id.nameTextView)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun showName(view: View) {
+        val name = nameEditText.text.toString()
+        nameOutputTextView.text = name
     }
 }
